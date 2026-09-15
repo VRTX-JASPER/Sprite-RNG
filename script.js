78efe388fefbb8111d852980b79ae2d280ac3199
VRTX-JASPER <611119@cjuhsd.net>
2026-09-15 02:56:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
+let rolls = 0;
 
+const items = [
+    {
+        name: "Common",
+        chance: 60
+    },
+
+    {
+        name: "Uncommon",
+        chance: 25
+    },
+
+    {
+        name: "Rare",
+        chance: 10
+    },
+
+    {
+        name: "Epic",
+        chance: 4
+    },
+
+    {
+        name: "Legendary",
+        chance: 1
+    }
+];
+
+
+function roll() {
+
+    rolls++;
+
+    document.getElementById("rolls").textContent = rolls;
+
+    let random = Math.random() * 100;
+
+    let total = 0;
+
+    for (let item of items) {
+
+        total += item.chance;
+
+        if (random <= total) {
+
+            document.getElementById("result").textContent =
+                "✨ " + item.name + " ✨";
+
+            break;
+        }
+    }
+}
